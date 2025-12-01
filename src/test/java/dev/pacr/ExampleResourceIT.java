fe3ae7ffb509a4d30ec33b7d918c87f20c0fd262
Patrick <pcr@doggo.dance>
2025-12-01 15:41:56 -0600

    Fix

--- src/test/java/dev/pacr/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package dev.pacr;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-	// Execute the same tests but in packaged mode.
-}
